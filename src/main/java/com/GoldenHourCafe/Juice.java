928bf6cd1a22603e4db3fbf6b57607b1a5520c70
daliseb <odaliseb59@gmail.com>
2025-11-10 15:36:48 -0500

    juice class

--- src/main/java/com/GoldenHourCafe/Juice.java
+++ src/main/java/com/GoldenHourCafe/Juice.java
@@ -1,4 +1,8 @@
 package com.GoldenHourCafe;
 
 public class Juice {
+    private Boolean wantDrink;  //drink is Not required, ask the user if wanted.
+    private String type;   //what drink?
+    private String price;
+
 }
