387413ba9fc221874038a9ef44814e7c29512b91
daliseb <odaliseb59@gmail.com>
2025-11-10 13:58:56 -0500

    added my constructors, getters, setters and toString

--- src/main/java/com/GoldenHourCafe/SmoothieBase.java
+++ src/main/java/com/GoldenHourCafe/SmoothieBase.java
@@ -5,8 +5,47 @@ import javax.swing.*;
 public class SmoothieBase {
     private String size;
     private String baseType;
+    private double price;
 
 
+    //constructors
+    public SmoothieBase(String size, String baseType, double price) {
+        this.size = size;
+        this.baseType = baseType;
+        this.price = price;
+    }
 
+    //getters and setters
+    public String getSize() {
+        return size;
+    }
 
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getBaseType() {
+        return baseType;
+    }
+
+    public void setBaseType(String baseType) {
+        this.baseType = baseType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "SmoothieBase{" +
+                "size='" + size + '\'' +
+                ", baseType='" + baseType + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
