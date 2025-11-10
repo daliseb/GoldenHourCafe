cfdbd7aec96dfba292308d5cd3d7208c9d7a80e9
daliseb <odaliseb59@gmail.com>
2025-11-10 14:26:13 -0500

    finished topping class

--- src/main/java/com/GoldenHourCafe/Topping.java
+++ src/main/java/com/GoldenHourCafe/Topping.java
@@ -1,9 +1,47 @@
 package com.GoldenHourCafe;
 
 public class Topping {
-    private String name;
-    private Boolean type; //two types of toppings, premium/regular
-    private Boolean extra;
-    private double price;
+    private String name;  //name of topping
+    private String type; //two types of toppings, premium/regular
+    private Boolean extra;   //extra topping
+    private double price;    //base price of topping
 
+    public Topping(String name, String type, Boolean extra, double price) {
+        this.name = name;
+        this.type = type;
+        this.extra = extra;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getExtra() {
+        return extra;
+    }
+
+    public void setExtra(Boolean extra) {
+        this.extra = extra;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
