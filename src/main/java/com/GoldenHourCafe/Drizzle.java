6e236382351760ff080d2aefc16b42454d02d463
daliseb <odaliseb59@gmail.com>
2025-11-10 16:05:51 -0500

    added drizzle class

--- src/main/java/com/GoldenHourCafe/Drizzle.java
+++ src/main/java/com/GoldenHourCafe/Drizzle.java
@@ -3,6 +3,38 @@ package com.GoldenHourCafe;
 public class Drizzle {
     private Boolean wantDrizzle; //option for drizzle
     private String type;  //what kind
-    private 
+    private double price;
 
+
+    public Drizzle(Boolean wantDrizzle, String type, double price) {
+        this.wantDrizzle = wantDrizzle;
+        this.type = type;
+        this.price = price;
+    }
+
+    public Boolean getWantDrizzle() {
+        return wantDrizzle;
+    }
+
+    public void setWantDrizzle(Boolean wantDrizzle) {
+        this.wantDrizzle = wantDrizzle;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //to do, add override, fix string for price.
 }
