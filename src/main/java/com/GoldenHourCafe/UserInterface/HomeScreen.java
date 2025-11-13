3fd17bccbb22ad28e53e8bed5a1b9aefc6340953
daliseb <odaliseb59@gmail.com>
2025-11-12 22:39:51 -0500

    starting order screen

--- src/main/java/com/GoldenHourCafe/UserInterface/HomeScreen.java
+++ src/main/java/com/GoldenHourCafe/UserInterface/HomeScreen.java
@@ -1,4 +1,18 @@
 package com.GoldenHourCafe.UserInterface;
 
+//this is my main screen
 public class HomeScreen {
+    public void homeScreenDisplay(){
+        while(true){
+            System.out.println("""
+                    ---Welcome to Golden Hour Cafe---
+                       what would you like to do?
+                       1. Make An Order
+                       2. Exit
+                    """);
+            }
+        }
+
+    }
+
 }
--- src/main/java/com/GoldenHourCafe/UserInterface/Order.java
+++ src/main/java/com/GoldenHourCafe/UserInterface/Order.java
@@ -1,6 +1,17 @@
 package com.GoldenHourCafe.UserInterface;
 
 public class Order {
+    private Order order = new Order();
+
+    public void startOrder(){
+        homeScreen();
+
+    }
+
+    //home screen(order screen) starts here:
+
+
+
 
 
 }
