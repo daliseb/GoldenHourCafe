329b8fd50a3dfc90219ef7b4e0315dbedf882a19
daliseb <odaliseb59@gmail.com>
2025-11-13 18:04:33 -0500

    trying to get my receipts running

--- src/main/java/com/GoldenHourCafe/Order/ReceiptFileManager.java
+++ src/main/java/com/GoldenHourCafe/Order/ReceiptFileManager.java
@@ -1,8 +1,10 @@
 package com.GoldenHourCafe.Order;
 
-import com.GoldenHourCafe.*;
 import com.GoldenHourCafe.ShopItems.*;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.*;
 import java.time.format.*;
 import java.util.ArrayList;
@@ -11,6 +13,26 @@ public class ReceiptFileManager {
     public static void writeReceipt(ArrayList<SmoothieBase> bowls, ArrayList<Juice> juices, ArrayList<Side> sides, double totalPrice){
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String time = now.format(timeFormatter);
+
+        // Creating a file name:
+        String filename = time + ".txt";
+        try {
+            FileWriter fileW = new FileWriter(filename);
+            BufferedWriter bw = new BufferedWriter(fileW);
+
+            bw.write(time);
+
+            if(!bowls.isEmpty()){
+                bw.write("\n ----- SMOOTHIE BOWLS -----\n");
+                for (SmoothieBase bowl : bowls) {
+                    bw.write(String.valueOf(bowl));
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
