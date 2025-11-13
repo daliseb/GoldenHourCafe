f2e5ae9b5e0b2a7c16c3b1d9e01bf081016d56ed
daliseb <odaliseb59@gmail.com>
2025-11-13 11:30:06 -0500

    adding while loop for side class

--- src/main/java/com/GoldenHourCafe/ShopItems/Side.java
+++ src/main/java/com/GoldenHourCafe/ShopItems/Side.java
@@ -1,5 +1,9 @@
 package com.GoldenHourCafe.ShopItems;
 
+import com.GoldenHourCafe.UserInterface.ConsoleHelper;
+
+import javax.print.attribute.standard.Sides;
+
 public class Side {
     private Boolean wantSide; //a question, sides are NOT required.
     private String type;
@@ -37,8 +41,33 @@ public class Side {
         this.price = price;
     }
 
-    @Override
-    public String toString() {
-        return wantSide + " " + type + " " + price;
+    //prompt the user, using the same structure from my other classes:
+    public static Side optionForSide() {
+        System.out.println("""
+                -----Choose A Side-----
+                      (3.00 Each)
+                1. Banana Oat Muffin
+                2. Chocolate Protein Bites""");
+
+
+        boolean wantSide = ConsoleHelper.promptForBoolean("Add A Side? (y/n)");
+        if (!wantSide) {
+            System.out.println("No Side Added");
+            return new Side(false, "None", 0.00);
+        }
+
+        int choice;
+        while(true){
+            choice = ConsoleHelper.promptForInt("Select Your Side (1-2)");
+            if (choice >= 1 && choice <= 2) break;
+            System.out.println("Invalid Choice, Select (1-2)");
+        }
+
+
     }
+//    @Override
+//    public String toString() {
+//        return wantSide + " " + type + " " + price;
+//    }
 }
+
