3f54f99c60a91753d59a8b0332392ca8aa23e759
daliseb <odaliseb59@gmail.com>
2025-11-13 11:07:46 -0500

    adding case/switch for drizzle

--- src/main/java/com/GoldenHourCafe/ShopItems/Drizzle.java
+++ src/main/java/com/GoldenHourCafe/ShopItems/Drizzle.java
@@ -29,8 +29,8 @@ public class Drizzle {
         this.type = type;
     }
 
-        public static Drizzle optionForDrizzle(String type) {
-            System.out.println("""
+    public static Drizzle optionForDrizzle(String type) {
+        System.out.println("""
                 ----Select Your Drizzle!----
                 1.Peanut Butter
                 2.Almond Butter
@@ -40,56 +40,47 @@ public class Drizzle {
                 6.None
                 """);
 
-            boolean wantDrizzle = ConsoleHelper.promptForBoolean("Would You Like Drizzle? (y/n)");
-            //following if statement, is made in case user wants no drizzle
-            if (!wantDrizzle) {
-                System.out.println("No Drizzle Added");
-                return new com.GoldenHourCafe.ShopItems.Drizzle(false, "None");
-            }
-            int choice;
-            while (true) {
-                choice = ConsoleHelper.promptForInt("Enter Your Drizzle Number! (please type 1-6");
-                if (choice >= 1 && choice <= 6) break;
-                System.out.println("Invalid Selection! Try Again....");
-            }
-
-            String drizzleType = getDrizzleName(choice);
-            System.out.println("Juice Has Been Added To Cart!" + " " + type + " " + price
-            return
-
-
-
-
-            public void int getDrizzleName (String type){
-                switch (type) {
-                    case 1 -> {
-                        type = "Peanut Butter";
-                    }
-                    case 2 -> {
-                        type = "Almond Butter";
-                    }
-                    case 3 -> {
-                        type = "Honey";
-                    }
-                    case 4 -> {
-                        type = "Organic Greek Yogurt";
-                    }
-                    case 5 -> {
-                        type = "Agave";
-                    }
-                    default -> type = "No Drizzle Added";
-                }
-                ;
-                //TO DO: FINISH FIXING SWITCH CASE AND FINISH WRITING LOGIC FOR CHOICES
-                System.out.println();
-
-
-            }
-
+        boolean wantDrizzle = ConsoleHelper.promptForBoolean("Would You Like Drizzle? (y/n)");
+        //following if statement, is made in case user wants no drizzle
+        if (!wantDrizzle) {
+            System.out.println("No Drizzle Added");
+            return new com.GoldenHourCafe.ShopItems.Drizzle(false, "None");
         }
-        @Override
-        public String toString () {
-            return wantDrizzle + " " + type;
+        int choice;
+        while (true) {
+            choice = ConsoleHelper.promptForInt("Enter Your Drizzle Number! (please type 1-6");
+            if (choice >= 1 && choice <= 6) break;
+            System.out.println("Invalid Selection! Try Again....");
         }
+
+        //following same structure as Juice class:
+        String drizzleType = getDrizzleName(choice);
+        System.out.println("Drizzle Has Been Added!" + " " + type);
+        return new Drizzle(true, drizzleType);
     }
+        //switch case here to convert answers to a numerical value:
+        private static String getDrizzleName(int choice) {
+            return switch(choice){
+                case 1
+                case 2
+                case 3
+                case 4
+                case 5
+
+
+            };
+
+
+        }
+
+
+
+//        @Override
+//        public String toString () {
+//    return wantDrizzle + " " + type;
+//}
+
+            }
+
+
 
